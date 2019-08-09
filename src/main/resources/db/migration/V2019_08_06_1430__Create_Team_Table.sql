bcb508bdcea1a3509494aba313039761503843a7
A817256 <andreassen.spenser@principal.com>
2019-08-09 08:11:23 -0500

    chore: populate data after table creation

--- src/main/resources/db/migration/V2019_08_06_1430__Create_Team_Table.sql
+++ src/main/resources/db/migration/V2019_08_06_1430__Create_Team_Table.sql
@@ -1,6 +1,15 @@
 create table spenserca.Team (
-    Id          int         not null identity primary key,
-    Name        varchar(64) not null,
-    Description varchar(64) null,
-    CreatedDate datetime    not null constraint df_Team_CreatedDate default (getdate())
+                                Id          int         not null primary key,
+                                Name        varchar(64) not null,
+                                Description varchar(64) null,
+                                CreatedDate datetime    not null constraint df_Team_CreatedDate default (getdate())
 );
+
+insert into spenserca.Team
+select Id,
+       [Name],
+       [Description],
+       CreatedDate
+from (values (1, 'Bakers Dozen', 'Non-Qualified Team 9', getdate()),
+             (2, 'Fully Qualified', 'Non-Qualified Team 8', getdate())
+     ) v (Id, Name, Description, CreatedDate);
