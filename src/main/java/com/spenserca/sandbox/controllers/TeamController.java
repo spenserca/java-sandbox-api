8158de2fc8e6992ec5a070f94d98b0ba04016123
A817256 <andreassen.spenser@principal.com>
2019-08-09 08:49:49 -0500

    feat: add annotations to wire up controller and di

--- src/main/java/com/spenserca/sandbox/controllers/TeamController.java
+++ src/main/java/com/spenserca/sandbox/controllers/TeamController.java
@@ -3,16 +3,23 @@ package com.spenserca.sandbox.controllers;
 import com.spenserca.sandbox.models.Team;
 import com.spenserca.sandbox.repositories.TeamRepository;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
+import javax.inject.Inject;
 import java.util.List;
 
+@RestController
 public class TeamController {
     private TeamRepository teamRepository;
 
+    @Inject
     public TeamController(TeamRepository teamRepository) {
         this.teamRepository = teamRepository;
     }
 
+    @RequestMapping(method = RequestMethod.GET, path = "/teams")
     public ResponseEntity<List<Team>> get() {
         List<Team> teams;
 
--- src/main/java/com/spenserca/sandbox/repositories/TeamRepository.java
+++ src/main/java/com/spenserca/sandbox/repositories/TeamRepository.java
@@ -1,10 +1,12 @@
 package com.spenserca.sandbox.repositories;
 
 import com.spenserca.sandbox.models.Team;
+import org.springframework.stereotype.Component;
 
 import java.util.Collections;
 import java.util.List;
 
+@Component
 public class TeamRepository {
     public List<Team> getTeams() {
         return Collections.emptyList();
