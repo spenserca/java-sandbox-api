d89ae7b38d28cd579203015786bea58dade408fa
spenserca <spenserca@gmail.com>
2019-11-26 15:31:49 -0600

    feat: auto generate identity and modified timestamp

--- src/main/java/com/spenserca/sandbox/models/dao/TeamDao.java
+++ src/main/java/com/spenserca/sandbox/models/dao/TeamDao.java
@@ -3,6 +3,8 @@ package com.spenserca.sandbox.models.dao;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
@@ -17,7 +19,7 @@ public class TeamDao {
     @Id
     @NotNull
     @Column(name = "Id")
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     @NotNull
@@ -27,7 +29,7 @@ public class TeamDao {
     @Column(name = "Description")
     private String description;
 
+    @UpdateTimestamp
     @Column(name = "ModifiedDate")
-    @GeneratedValue(strategy = GenerationType.AUTO)
     private Timestamp modifiedDate;
 }
